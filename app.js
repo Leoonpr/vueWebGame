b4d5026ef5115f5930db51ee9695c777d98bda0e
Leonardo <rochaleo879@gmail.com>
2024-07-10 15:52:54 -0300

    feat: Finishing the Core Functionality

--- app.js
+++ app.js
@@ -14,9 +14,15 @@ const app = Vue.createApp({
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+       return {width: '0%'};
+      }
       return {width: this.monsterHealth + '%'};
     },
     playerBarStyles() {
+      if(this.playerHealth < 0) {
+        return {width: '0%'};
+      }
       return {width: this.playerHealth + '%'};
     },
     mayUseSpecialAttack() {
@@ -49,6 +55,13 @@ const app = Vue.createApp({
     },
   },
   methods: {
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 0;
+      this.currentHeals = 0;
+      this.winner = null;
+    },
     attackMonster() {
       this.currentRound++;
       const damage = getRandomValue(5, 12);
@@ -74,6 +87,9 @@ const app = Vue.createApp({
       }
       this.playerHealth += heal;
       this.attackPlayer();
+    },
+    surrender() {
+      this.winner ='monster';
     }
   }
 })
